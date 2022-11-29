bd063af4f9875b468babad9962e420d430d2b0d9
Bruno Wowk <bruno.wowk@gmail.com>
2022-11-29 10:40:27 -0300

    enable swiping on touch controls only

--- pages/chapters/[chapterNumber]/[section].tsx
+++ pages/chapters/[chapterNumber]/[section].tsx
@@ -158,13 +158,16 @@ const ChapterSection: FC<ChapterSectionProps> = ({
               {...pixelBoxProps}
               // @ts-ignore
               onPanEnd={(event, info) => {
-                const {delta, offset} = info
-                console.log({delta: delta.x, offset: offset.x})
-                if (info.offset.x < -90 && nextSection) {
+                if (event.pointerType !== "touch") {
+                  return
+                }
+                const {delta, offset, velocity } = info
+                console.log({delta: delta.x, offset: offset.x, velocity: velocity.x})
+                if (info.velocity.x < -100 && nextSection) {
                   push(nextSection)
                 }
 
-                if (info.offset.x > 90 && previousSection) {
+                if (info.velocity.x > 100 && previousSection) {
                   push(previousSection)
                 }
               }}
