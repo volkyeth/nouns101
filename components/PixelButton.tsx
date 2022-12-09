c4cd9137284f45cc63a13f55ad623eefc826ea45
Bruno Wowk <bruno.wowk@gmail.com>
2022-12-09 10:18:56 -0300

    Remove CTA outline

--- components/PixelButton.tsx
+++ components/PixelButton.tsx
@@ -2,23 +2,17 @@ import {
   Button,
   ButtonProps,
   Center,
-  chakra,
   forwardRef,
   SimpleGrid,
 } from "@chakra-ui/react";
-import { FC, ForwardedRef } from "react";
+import { FC } from "react";
 import { pixelatedClipPath } from "../utils/clipPaths";
-import { Shadow } from "./Shadow";
 import { PixelBox } from "./ShadowedPixelBox";
-import { ConditionalWrapper } from "./ConditionalWrapper";
 
 export type PixelButtonProps = {
   pixelSize?: number;
   shadowColor?: string;
-  outlineColor?: string;
-  outlineSize?: number;
   hasShadow?: boolean;
-  hasOutline?: boolean;
 } & ButtonProps;
 
 export const PixelButton = forwardRef<PixelButtonProps, "button">(
@@ -26,9 +20,7 @@ export const PixelButton = forwardRef<PixelButtonProps, "button">(
     {
       pixelSize = 2,
       shadowColor = "black",
-      outlineColor = "black",
       hasShadow = true,
-      hasOutline = false,
       alignSelf,
       children,
       ...props
@@ -47,57 +39,28 @@ export const PixelButton = forwardRef<PixelButtonProps, "button">(
           transform={`translate(${pixelSize * 2}px, ${pixelSize * 2}px)`}
         />
       )}
-      <ConditionalWrapper
-        wrap={hasOutline}
-        wrapper={(children) => (
-          <PixelBox
-            p={`${pixelSize}px`}
-            gridArea={"1/1/1/1"}
-            bgColor={"black"}
-            pixelSize={pixelSize}
-            _hover={{
-              transform: `translate(1px, 1px)`,
-            }}
-            _active={{
-              transform: `translate(${pixelSize * 2}px, ${pixelSize * 2}px)`,
-            }}
-          >
-            {children}
-          </PixelBox>
-        )}
+
+      <Button
+        variant={"unstyled"}
+        gridArea={"1/1/1/1"}
+        ref={ref}
+        color={"#2245C5"}
+        fontFamily={`"Lores 12 OT", sans-serif;`}
+        clipPath={pixelatedClipPath(pixelSize)}
+        bgColor={"white"}
+        h={7}
+        w={"fit-content"}
+        minW={20}
+        _hover={{
+          transform: `translate(1px, 1px)`,
+        }}
+        _active={{
+          transform: `translate(${pixelSize * 2}px, ${pixelSize * 2}px)`,
+        }}
+        {...props}
       >
-        <Button
-          variant={"unstyled"}
-          gridArea={"1/1/1/1"}
-          ref={ref}
-          color={"#2245C5"}
-          fontFamily={`"Lores 12 OT", sans-serif;`}
-          clipPath={pixelatedClipPath(pixelSize)}
-          bgColor={"white"}
-          h={7}
-          w={"fit-content"}
-          minW={20}
-          _hover={
-            !hasOutline
-              ? {
-                  transform: `translate(1px, 1px)`,
-                }
-              : undefined
-          }
-          _active={
-            !hasOutline
-              ? {
-                  transform: `translate(${pixelSize * 2}px, ${
-                    pixelSize * 2
-                  }px)`,
-                }
-              : undefined
-          }
-          {...props}
-        >
-          <Center>{children}</Center>
-        </Button>
-      </ConditionalWrapper>
+        <Center>{children}</Center>
+      </Button>
     </SimpleGrid>
   )
 );
@@ -111,7 +74,6 @@ export const CtaButton: FC<CtaButtonProps> = ({
   ...props
 }) => (
   <PixelButton
-    hasOutline
     color={colorScheme === "blue" ? "white" : undefined}
     bgColor={colorScheme === "blue" ? "#254EFB" : undefined}
     {...props}
