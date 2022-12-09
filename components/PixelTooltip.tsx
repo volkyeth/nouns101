ef197106eeb14bb11c8118107edc38c28222392c
Bruno Wowk <bruno.wowk@gmail.com>
2022-12-09 11:04:01 -0300

    Use pixel font on tooltips

--- components/PixelTooltip.tsx
+++ components/PixelTooltip.tsx
@@ -3,6 +3,7 @@ import { chakra, forwardRef, Tooltip, TooltipProps } from "@chakra-ui/react";
 export const PixelTooltip = forwardRef<TooltipProps, "div">(
   ({ children, ...props }, ref) => (
     <Tooltip
+      fontFamily={`"LoRes 12 OT",sans-serif`}
       hasArrow
       color={"white"}
       bgColor={"nouns101.blue"}
