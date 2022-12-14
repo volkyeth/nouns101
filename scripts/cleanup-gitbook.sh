f965d3a52eb78a8e024c44a6404aceac181d3ec4
Bruno Wowk <bruno.wowk@gmail.com>
2022-12-14 09:09:14 -0300

    Update script

--- scripts/cleanup-gitbook.sh
+++ scripts/cleanup-gitbook.sh
@@ -5,19 +5,28 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/../content"
 
 # remove readme files and summary
-rm **/gitbook-readme.md
-rm gitbook-summary.md
+rm **/README.md
+# @TODO replace after first cleanup:
+#rm **/gitbook-readme.md
+#rm gitbook-summary.md
 
 # move assets to the public folder
 mv $SCRIPT_DIR/../.gitbook/assets/* $SCRIPT_DIR/../public/assets/
 
+# Process new files
+ADD_FRONTMATTER='s/# (.+)/---\ntitle: \1\naliases: []\nseeAlso: []\nexternalReferences: {}\n---/'
+REMOVE_HEADINGS='s/^# .+//g'
+find . -name "*.md" -exec sed -i '' -E "$ADD_FRONTMATTER;$REMOVE_HEADINGS" {} +
+# Remove duplicated line breaks
+find . -name "*.md" -exec sed -i '' '/^$/N;/^\n$/D' {} +
+
 # change md files to mdx
 find . -name "*.md" -exec rename 's/\.md$/.mdx/' '{}' +
 
+
 # Cleanup all files
-ADD_FRONTMATTER='s/# (.+)/---\ntitle: \1\naliases: []\nseeAlso: []\nexternalReferences: {}\n---/'
 REMOVE_TRAILING_SLASH='s/\\$//'
 FIX_IMAGES='s/!\[]\(<.+\/.gitbook\/assets\/(.+)>\)/!\[](\/assets\/\1)/g'
 UNESCAPE_WIKILINKS='s/\\\[\\\[/[[/g'
 REMOVE_EMBEDS='s/\{% embed url="(.+)" %\}/\1/g'
-find . -name "*.mdx" -exec sed -i '' -E "$ADD_FRONTMATTER; $REMOVE_TRAILING_SLASH; $FIX_IMAGES;$UNESCAPE_WIKILINKS;$REMOVE_EMBEDS" {} +
+find . -name "*.mdx" -exec sed -i '' -E "$REMOVE_TRAILING_SLASH; $FIX_IMAGES;$UNESCAPE_WIKILINKS;$REMOVE_EMBEDS" {} +
