dbc9e4bf05116261deca956e654ee8b52ab6055c
Bruno Wowk <bruno.wowk@gmail.com>
2022-12-14 08:31:12 -0300

    Update cleanup script

--- scripts/cleanup-gitbook.sh
+++ scripts/cleanup-gitbook.sh
@@ -11,15 +11,13 @@ rm gitbook-summary.md
 # move assets to the public folder
 mv $SCRIPT_DIR/../.gitbook/assets/* $SCRIPT_DIR/../public/assets/
 
-# Cleanup new files
-ADD_FRONTMATTER='s/# (.+)/---\ntitle: \1\naliases: []\nseeAlso: []\nexternalReferences: {}\n---/'
-REMOVE_TRAILING_SLASH='s/\\$//'
-REMOVE_EMBEDS='s/\{% embed url="(.+)" %\}/\1/g'
-FIX_IMAGES='s/!\[]\(<.+\/.gitbook\/assets\/(.+)>\)/!\[](\/assets\/\1)/g'
-find . -name "*.md" -exec sed -i '' -E "$ADD_FRONTMATTER; $REMOVE_TRAILING_SLASH; $REMOVE_EMBEDS; $FIX_IMAGES" {} +
-
 # change md files to mdx
 find . -name "*.md" -exec rename 's/\.md$/.mdx/' '{}' +
 
-# unescape wikilinks all over the place
-find . -name "*.mdx" -exec sed -i '' 's/\\\[\\\[/[[/g' '{}' +
+# Cleanup all files
+ADD_FRONTMATTER='s/# (.+)/---\ntitle: \1\naliases: []\nseeAlso: []\nexternalReferences: {}\n---/'
+REMOVE_TRAILING_SLASH='s/\\$//'
+FIX_IMAGES='s/!\[]\(<.+\/.gitbook\/assets\/(.+)>\)/!\[](\/assets\/\1)/g'
+UNESCAPE_WIKILINKS='s/\\\[\\\[/[[/g'
+REMOVE_EMBEDS='s/\{% embed url="(.+)" %\}/\1/g'
+find . -name "*.mdx" -exec sed -i '' -E "$ADD_FRONTMATTER; $REMOVE_TRAILING_SLASH; $FIX_IMAGES;$UNESCAPE_WIKILINKS;$REMOVE_EMBEDS" {} +
