3db5a7747ac58f9517d3faff2b228a216fa5272e
Bruno Wowk <bruno.wowk@gmail.com>
2022-12-20 07:46:46 -0300

    Gitbook fixes

--- scripts/cleanup-gitbook.sh
+++ scripts/cleanup-gitbook.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/../content"
 
 # remove readme files and summary
-find . -name "gitbook-readme.md" -type f -delete
-rm gitbook-summary.md
+find . -name README.md -type f -delete
+rm ../SUMMARY.md
 
 # move assets to the public folder
 mv $SCRIPT_DIR/../.gitbook/assets/* $SCRIPT_DIR/../public/assets/
