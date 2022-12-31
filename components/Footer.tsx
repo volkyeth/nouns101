223347134438f8d62d5353596585db23edda774f
Volky <volkyeth@proton.me>
2022-12-31 07:06:21 -0300

    Powered by Vercel

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
 import logo from "../assets/noggle101.svg";
-import { Center, CenterProps } from "@chakra-ui/react";
+import { Center, CenterProps, HStack, Link } from "@chakra-ui/react";
 import { FC } from "react";
 import { MainContainer } from "./MainLayout";
 
 export const Footer: FC<CenterProps> = (props) => (
   <Center bgColor={"#FFE7BF"} w={"full"} {...props}>
     <MainContainer>
-      <Image src={logo} alt={"logo"} />
+      <HStack justifyContent={"space-between"}>
+        <Image src={logo} alt={"logo"} />
+        <Link
+          href={"https://vercel.com/?utm_source=Nouns101&utm_campaign=oss"}
+          isExternal
+          fontFamily={`"LoRes 12 OT",sans-serif`}
+        >
+          Powered by ▲
+        </Link>
+      </HStack>
     </MainContainer>
   </Center>
 );
