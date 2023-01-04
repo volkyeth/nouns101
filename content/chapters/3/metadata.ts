2cbcba0c4fd40d9140c75e12c081cce68b74f904
Volky <volkyeth@proton.me>
2023-01-04 16:13:17 -0300

    Change ch3 title
    Closes #42

--- content/chapters/3/metadata.ts
+++ content/chapters/3/metadata.ts
@@ -3,7 +3,7 @@ import noggles from "./noggles.svg";
 
 export default {
   id: "3",
-  title: "NFTs and DAOs",
+  title: "Types of NFTs",
   color: "#DAE6FF",
   image: noggles,
 } as ChapterMetadata;
