f36fa3e8bda2f42c98d5ac41e8b38fe4baecd7ed
Volky <volkyeth@proton.me>
2023-01-10 08:17:36 -0300

    Remove serialize page

--- pages/api/serialize.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { serializeMdx } from "../../utils/mdx";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(await serializeMdx(req.body));
-};
