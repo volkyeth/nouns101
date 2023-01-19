1274cdac7984acc6daf219d17245dc471bffe444
Volky <volkyeth@proton.me>
2023-01-19 16:12:12 -0300

    Add Google Tag Manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,34 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { chakra } from "@chakra-ui/react";
+import Script from "next/script";
 
 export default function Document() {
+  const gaMeasurementId = process.env.GA_MEASUREMENT_ID;
   return (
     <Html>
       <Head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <script
-          async
-          src="https://platform.twitter.com/widgets.js"
-          charSet="utf-8"
-        ></script>
+        {gaMeasurementId && (
+          <Script>
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gaMeasurementId}');`}
+          </Script>
+        )}
       </Head>
       <body>
+        {gaMeasurementId && (
+          <noscript>
+            <chakra.iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gaMeasurementId}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></chakra.iframe>
+          </noscript>
+        )}
         <Main />
         <NextScript />
       </body>
