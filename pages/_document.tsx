5f009fbba4c05e2a4d82d37a200576cdb30b082a
Volky <volkyeth@proton.me>
2023-01-19 16:15:25 -0300

    Add id to script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
       <Head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         {gaMeasurementId && (
-          <Script>
+          <Script id="gtm">
             {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
