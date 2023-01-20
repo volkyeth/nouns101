55bbd8ec9de76e619a200383d542e598fbd251bf
Volky <volkyeth@proton.me>
2023-01-20 05:56:51 -0300

    Move gtm to _app to ensure it loads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,22 @@ import "@fontsource/nunito";
 import "../styles/globals.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
     <>
+      {process.env.GA_MEASUREMENT_ID && (
+        <Script id="gtm" strategy={"beforeInteractive"}>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.GA_MEASUREMENT_ID}');`}
+        </Script>
+      )}
       <QueryClientProvider client={queryClient}>
         <ChakraProvider theme={theme}>
           <Fonts />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,21 +8,12 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        {gaMeasurementId && (
-          <Script id="gtm">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${gaMeasurementId}');`}
-          </Script>
-        )}
       </Head>
       <body>
-        {gaMeasurementId && (
+        {process.env.GA_MEASUREMENT_ID && (
           <noscript>
             <chakra.iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${gaMeasurementId}`}
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.GA_MEASUREMENT_ID}`}
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
